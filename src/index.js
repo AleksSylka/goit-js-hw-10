907dcbbc837a72c6e6e79d21a49fe42a421a6c4c
AleksSylka <119104646+AleksSylka@users.noreply.github.com>
2023-04-23 20:57:46 +0300

    HW-10-3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 import './css/styles.css';
-
+import Notiflix from 'notiflix';
+var debounce = require('lodash.debounce');
+import { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('input');
+const ulEl = document.querySelector('.country-list');
+const divEl = document.querySelector('.country-info')
+
+inputEl.addEventListener('input', debounce(onArrayCountry, DEBOUNCE_DELAY))
+
+function onArrayCountry() {
+    let name = inputEl.value.trim();
+    
+    fetchCountries(name).then(arrayCountries => {
+        if (arrayCountries.length > 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+        } else if (arrayCountries.length === 1) {
+            ulEl.innerHTML = ''
+            divEl.innerHTML = createMarkupCountry(arrayCountries);
+        } else {
+            divEl.innerHTML = ''
+            ulEl.innerHTML = createMarkupList(arrayCountries);
+        }
+    }
+    ).catch(err => console.log(err));
+}
+
+function createMarkupList(arr) {
+    return arr.map(({ flags: { svg }, name: { official } }) => `<li class="js-item">
+        
+        <p class="js-name">
+        <img class="js-img-card" src="${svg}" alt="Flag of ${official}">
+        ${official}</p>
+      </li>`).join("");
+};
+
+function createMarkupCountry(arr) {
+    const {flags: { svg }, name: { official }, capital, languages, population } = arr[0];
+    return `
+      <h2 class="js-title-card"><img class="js-img-card" src="${svg}" alt="Flag of ${official}">${official}</h2>
+      <p class="js-text-card"><b>Capital:</b> ${capital}</p>
+      <p class="js-text-card"><b>Population:</b> ${population}</p>
+      <p class="js-text-card"><b>Languages:</b> ${Object.values(languages)}</p>`
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+import Notiflix from 'notiflix';
+
+function fetchCountries(name) {
+    const BASE_URL = 'https://restcountries.com/v3.1/name';
+    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`).then(resp => {
+        if (!resp.ok) {
+            throw new Error(Notiflix.Notify.failure('Oops, there is no country with that name'));
+            
+        }
+       return resp.json()
+    })
+};
+
+export { fetchCountries };
